103dad463700ebee3e889eb4ccf2dc8eef7520e5
Sergey Popov <spopov@shakuro.com>
2022-02-21 06:57:31 +0300

    II-15-[removed pods]

--- Pods/Target Support Files/Pods-InfinityScrollView_Framework/Pods-InfinityScrollView_Framework-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_InfinityScrollView_Framework : NSObject
-@end
-@implementation PodsDummy_Pods_InfinityScrollView_Framework
-@end
--- Pods/Target Support Files/Shakuro.CommonTypes/Shakuro.CommonTypes-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Shakuro_CommonTypes : NSObject
-@end
-@implementation PodsDummy_Shakuro_CommonTypes
-@end
